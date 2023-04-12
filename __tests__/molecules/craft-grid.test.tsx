b30618dd039e4435cac0b09c02f263fd6f47bae0
yohandevperera <yohanp@swivelgroup.com.au>
2023-04-12 10:30:19 +0530

    added the test cases

--- __tests__/molecules/craft-grid.test.tsx
+++ __tests__/molecules/craft-grid.test.tsx
@@ -1,38 +1,37 @@
 import "@testing-library/jest-dom";
 import React from "react";
 import AppRoutes from "../../src/components/organisms/app-routes";
-import { EmployeeType } from "../../src/services/employee";
+import { CraftType } from "../../src/services/crafts";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import configureStore from "../../src/redux/store/configureStore";
 import GridItem from "../../src/components/molecules/grid-item";
 import renderer from "react-test-renderer";
 
-
 // Sample data set to check the grid view
-const employee: EmployeeType = {
-  email: "Darrin_Rippin@gmail.com",
-  firstname: "Henri",
-  gender: "M",
-  lastname: "Rodriguez",
-  phone: "+94771277218",
-  photo: "https://randomuser.me/api/portraits/men/92.jpg",
-  _id: "63ff04ffb8e613034a3ea943",
+const craft: CraftType = {
+  _id: "6435a142030702dfc3032abe",
+  name: "Painted Wine Bottle Vases",
+  categoryId: "64229dfa1f396b074aea09c4",
+  description: "",
+  qty: 10,
+  price: 3000,
+  photo: "",
 };
 
 // the store instance created to access the redux store
 const store = configureStore();
 
 // test snapshot to validate the data grid view
-describe("Employee grid snapshot", () => {
-  it("should render the employee gird with the meta test data", () => {
+describe("Craft grid snapshot", () => {
+  it("should render the craft gird with the meta test data", () => {
     const domTree = renderer
       .create(
         <>
           <Provider store={store}>
             <BrowserRouter>
               <AppRoutes />
-              <GridItem item={employee} handleDelete={() => {}} />
+              <GridItem item={craft} handleAddToCart={() => {}} />
             </BrowserRouter>
           </Provider>
         </>
